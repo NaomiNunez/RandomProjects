2934abe13a4466bb7b838fd73cb253c4357f9f96
NaomiNunez <naominunez@gmail.com>
2020-07-22 23:26:32 -0700

    adding data for cards

--- my-kool-friends/src/App.js
+++ my-kool-friends/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Header from './components/Header';
+import Card from './components/Card';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Card />
       <Footer />
     </div>
   );
