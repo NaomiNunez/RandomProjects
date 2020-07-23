cbe60b6b22c716f8798b95d14e698a669147cfe8
NaomiNunez <naominunez@gmail.com>
2020-07-22 22:20:59 -0700

    adding files

---
+++ my-kool-friends/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+    return(
+        <div className='container'>
+            <footer className='footer'>
+                <span>
+                    © 2020 Developed by Naomi Nuñez. All Rights Reserved.
+                </span>
+            </footer>
+        </div>
+    )
+}
---
+++ my-kool-friends/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+import Button from '@material-ui/core/Button';
+
+export default function Header() {
+    return (
+        <div className="container" id="header">
+            <p>a cool site so show off my very cool friends</p>
+        </div>
+    )
+}
